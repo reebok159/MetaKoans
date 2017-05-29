e6402a3733e44607f8db2bd5876ae9009ac1e71e
Eugene V <kokokoi9@mail.ru>
2017-05-29 21:27:20 +0300

    Implement attribute method

---
+++ knowledge.rb
@@ -0,0 +1,31 @@
+
+  def attribute(arg, &block)
+      var_name = arg
+
+      if arg.is_a? (Hash)
+        var_name = arg.keys[0]
+      end
+
+        define_method :"#{var_name}" do
+          unless instance_variable_defined? :"@#{var_name}"
+            value = block ? (instance_eval &block) : (arg.is_a?(Hash) ? arg.values[0] : nil)
+            instance_variable_set(:"@#{var_name}", value)
+          end
+
+          instance_variable_get(:"@#{var_name}")
+        end
+
+        define_method :"#{var_name}?" do
+          return instance_variable_get(:"@#{var_name}") if instance_variable_defined? :"@#{var_name}"
+          false
+        end
+
+        define_method :"#{var_name}=" do |value|
+          instance_variable_set(:"@#{var_name}", value)
+        end
+=begin
+
+
+
+=end
+  end
